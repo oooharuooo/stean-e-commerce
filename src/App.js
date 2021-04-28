f03c31f6aac3b0fd937fa1adea3a4f446ee4f75f
Dat <haruthong@gmail.com>
2021-04-28 02:52:12 -0700

    fetched data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
 import "./App.css";
 
+const url = `https://api.rawg.io/api/games?key=`;
+const sort = `&metacritic=70&page_size=10`;
+const apiKey = `6a522a36e82046969ab7f5ef35c3d7f2`;
+
 function App() {
-  return <div className="App">
-    Test
-  </div>;
+	const [data, setData] = useState([]);
+
+	// Fetch Data
+	const fetchData = async () => {
+		const {
+			data: { results },
+		} = await axios.get(`${url}${apiKey}${sort}`);
+		setData(results);
+	};
+	useEffect(() => fetchData(), []);
+
+	const z = data.map((d) => {
+		const { name, id, metacritic, genres, short_screenshots } = d;
+		console.log(d);
+		return (
+			<React.Fragment key={id}>
+				<h1>{name}</h1>
+				<p>{metacritic}</p>
+				{genres.map((g) => (
+					<p>{g.name}</p>
+				))}
+			</React.Fragment>
+		);
+	});
+	return <div className="App">{z}</div>;
 }
 
 export default App;
